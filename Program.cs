f9b329a0a0e22bc7a5b5bc9ac5d1ef98c1afe7b9
Andrei Ruk <sloninc@mail.ru>
2023-07-14 21:24:31 +0300

    Добавлены регулярные выражения в логику приложеия.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
-﻿namespace HW9
+﻿using System.Text.RegularExpressions;
+namespace HW9
 {
     internal class Program
     {
+
+       static string equality = "a*x^2+b*x+c=0";
         /// <summary>
         /// Исходное положение стрелки меню
         /// </summary>
@@ -10,11 +13,10 @@
         {
             try
             {
-                string s = "a*x^2+b*x+c=0";
                 ConsoleKeyInfo ki;
                 bool ainput = true, binput = true, cinput = true;
                 selectedValue = 1;
-                double a = 0, b = 0, c = 0;
+                double a = 0, b = 0, c = 0,preb=0;
                 PrintMenu();
                 WriteCursor(selectedValue);
                 do
@@ -35,29 +37,101 @@
                             Console.WriteLine(new string(' ', 60));
                             Console.SetCursorPosition(0, selectedValue);
                             Console.Write($" {options[selectedValue - 1]}: {ki.KeyChar.ToString()}");
-                            //Console.Write(ki.KeyChar.ToString());
                             if (selectedValue == 1)
                             {
                                 a = double.Parse(ki.KeyChar.ToString() + Console.ReadLine());
                                 Console.SetCursorPosition(0, 0);
                                 Console.WriteLine(new string(' ', 60));
                                 Console.SetCursorPosition(0, 0);
-                                s = s.Remove(0, s.IndexOf('*'));
-                                s =s.Insert(0,a.ToString());
-                                Console.WriteLine(s);
+                                if (Regex.IsMatch(equality, @"^\S+\*x\^2"))
+                                {
+                                    if (a == 0)
+                                    {
+                                        Regex regex1 = new Regex(@"^\S+\*x\^2\+?");
+                                        equality = regex1.Replace(equality, "");
+                                    }
+                                    else
+                                    {
+                                        Regex regex = new Regex(@"^\S+\*x\^2");
+                                        equality = regex.Replace(equality, $"{a}*x^2");
+                                    }
+                                }
+                                else
+                                {
+                                    if (a == 0)
+                                    {
+                                        if (Regex.IsMatch(equality, @"^-?x\^2"))
+                                        {
+                                            Regex regex = new Regex(@"^-?x\^2\+?");
+                                            equality = regex.Replace(equality, $"");
+                                        }
+                                    }
+                                    else
+                                    {
+                                        if (Regex.IsMatch(equality, @"^-?x\^2"))
+                                        {
+                                            Regex regex = new Regex(@"^-?x\^2");
+                                            equality = regex.Replace(equality, $"{a}*x^2");
+                                        }
+                                        else equality = Regex.IsMatch(equality, @"^-\w") ? equality.Insert(0, $"{a}*x^2") : equality.Insert(0, $"{a}*x^2+");
+                                    }
+                                    
+                                }
+                                if (a == 1 || a == -1)
+                                    equality = (a == 1) ? equality.Replace("1*x^", "x^") : equality.Replace("-1*x^", "-x^");
+                                Console.WriteLine(equality);
                                 ainput = false;
                             }
                             else if(selectedValue == 2)
                             {
+                                
                                 b = double.Parse(ki.KeyChar.ToString() + Console.ReadLine());
                                 Console.SetCursorPosition(0, 0);
                                 Console.WriteLine(new string(' ', 60));
                                 Console.SetCursorPosition(0, 0);
-                                s = s.Remove(s.IndexOf('^')+2, s.LastIndexOf('*')-s.IndexOf('^')-2);
-                                if (b.ToString()[0]=='-')
-                                s = s.Insert(s.IndexOf('^')+2, b.ToString());
-                                else s = s.Insert(s.IndexOf('^') + 2, "+"+b.ToString());
-                                Console.WriteLine(s);
+                                Regex regex = (preb==1||preb==-1)? new Regex(@"(\+|\-)?x(?(?=\^)2)") : new Regex(@"(\+|\-)?[0-9b\,]+\*x(?(?=\^)2)");
+                                if (Regex.IsMatch(equality, @"x[^^]"))
+                                {
+                                    if (b == 0)
+                                    {
+                                        if (a == 0&& !ainput)
+                                            equality = cinput ? "c=0":$"{c}=0";
+                                        else
+                                            equality = regex.Replace(equality, "");
+                                    }
+                                    else
+                                    {
+                                        if(a == 0 && !ainput)
+                                        {
+                                            equality = (b > 0) ? regex.Replace(equality, (b == 1) ? $"x" : $"{b}*x") : regex.Replace(equality, (b == -1) ? $"-x" : $"{b}*x");
+                                        }
+                                        else
+                                        {
+                                            equality = (b > 0) ? regex.Replace(equality, (b == 1) ? $"+x" : $"+{b}*x") : regex.Replace(equality, (b == -1) ? $"-x" : $"{b}*x");
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    if (b == 0) { }
+                                    else
+                                    {
+                                        if (a == 0 && !ainput)
+                                            equality = (c > 0||cinput) ? equality.Insert(0, $"{b}*x+") : equality.Insert(0, $"{b}*x");
+
+                                        else if(ainput)
+                                            equality = (b > 0) ? equality.Insert("a*x^2".Length, $"+{b}*x") : equality.Insert("a*x^2".Length, $"{b}*x");
+                                        else
+                                            equality = equality.Insert($"{a}*x^2".Length, $"{b}*x");
+                                    }
+
+                                }
+                                if (b == 1||b==-1)
+                                {
+                                    equality = (b==1) ? regex.Replace(equality, "+x") : regex.Replace(equality, "-x");
+                                }
+                                Console.WriteLine(equality);
+                                preb = b;
                                 binput = false;
                             }
                             else
@@ -66,11 +140,35 @@
                                 Console.SetCursorPosition(0, 0);
                                 Console.WriteLine(new string(' ', 60));
                                 Console.SetCursorPosition(0, 0);
-                                s = s.Remove(s.LastIndexOf('x')+1, s.IndexOf('=')-s.LastIndexOf('x')-1);
-                                if (c.ToString()[0] == '-')
-                                    s = s.Insert(s.LastIndexOf('x') + 1, c.ToString());
-                                else s = s.Insert(s.LastIndexOf('x') + 1, "+" + c.ToString());
-                                Console.WriteLine(s);
+                                Regex regex = new Regex(@"(\+|\-)+(c?|[0-9]?\,?[0-9]+)=");
+                                if (Regex.IsMatch(equality, @"(\+|\-)+(c?|[0-9]?\,?[0-9]+)="))
+                                {
+                                    if (c == 0) 
+                                    {
+                                            equality = regex.Replace(equality, "=");
+                                    }
+                                    else 
+                                    {
+                                        if (a == 0 && !ainput && b==0 && !binput)
+                                        {
+                                            equality = regex.Replace(equality, $"{c}=");
+                                        }
+                                        else
+                                        {
+                                            equality = (c > 0) ? regex.Replace(equality, $"+{c}=") : regex.Replace(equality, $"{c}=");
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    if (c == 0) { }
+                                    else
+                                    {
+                                        equality = (c > 0) ? equality.Insert(equality.IndexOf('='), $"+{c}") : equality.Insert(equality.IndexOf('='), $"{c}");
+                                    }
+
+                                }
+                                Console.WriteLine(equality);
                                 cinput = false;
                             }
                             break;
@@ -81,9 +179,9 @@
                 QuadraticEquation func = new QuadraticEquation(a, b, c);
                 var result = func.Roots;
                 if (result.Count == 2)
-                    Console.WriteLine($"Корни уравнения {s} равны: x1={result[0]} x2={result[1]}");
+                    Console.WriteLine($"Корни уравнения {equality} равны: x1={result[0]} x2={result[1]}");
                 if(result.Count == 1)
-                    Console.WriteLine($"Корень уравнения {s} равен: x={result[0]}");
+                    Console.WriteLine($"Корень уравнения {equality} равен: x={result[0]}");
             }
             catch(DiscriminantException de)
             {
@@ -139,7 +237,7 @@
         /// </summary>
         private static void PrintMenu()
         {
-            Console.WriteLine("a*x^2+b*x+c=0");
+            Console.WriteLine(equality);
             for (var i = 0; i < options.Length; i++)
             {
                 Console.WriteLine($" {options[i]}:");
--- QuadraticEquation.cs
+++ QuadraticEquation.cs
@@ -8,37 +8,48 @@ namespace HW9
 {
     internal class QuadraticEquation
     {
-        double _discriminant;
+        double discriminant;
         double _a, _b, _c;
-        List<double> _roots = new();
+        List<double> roots = new();
         public QuadraticEquation(double a=0, double b=0, double c=0)
         {
             _a = a;
             _b = b;
             _c = c;
-            _discriminant = _b * _b - 4 * _a * _c;
-            _calcRoots();
+            discriminant = _b * _b - 4 * _a * _c;
+            calcRoots();
         }
-        public double Discriminant { get { return _discriminant; } }
-        public List<double> Roots { get { return _roots; } }
+        public double Discriminant { get { return discriminant; } }
+        public List<double> Roots { get { return roots; } }
 
-        void _calcRoots()
+
+        void calcRoots()
         {
-            if(_discriminant > 0)
+            if (_a == 0)
             {
-                double root1 = (-_b + Math.Sqrt(_discriminant)) / 2 * _a;
-                double root2 = (-_b - Math.Sqrt(_discriminant)) / 2 * _a;
-                _roots.Add(root1);
-                _roots.Add(root2);
+                if(_b==0)
+                    Console.WriteLine("Переменная может принимать любые значения");
+                else
+                {
+                    double root1 = -_c / _b;
+                    roots.Add(root1);
+                }
             }
-            else if(_discriminant == 0)
+            else if (discriminant > 0)
             {
-                double root1 = (-_b) / 2 * _a;
-                _roots.Add(root1);
+                double root1 = (-_b + Math.Sqrt(discriminant)) / (2 * _a);
+                double root2 = (-_b - Math.Sqrt(discriminant)) / (2 * _a);
+                roots.Add(root1);
+                roots.Add(root2);
+            }
+            else if (discriminant == 0)
+            {
+                double root1 = (-_b) / (2 * _a);
+                roots.Add(root1);
             }
             else
             {
-                throw new DiscriminantException(_discriminant);
+                throw new DiscriminantException(discriminant);
             }
         }
 
