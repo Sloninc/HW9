51b87bc4ac49b99856684ced282d5eb8cf335fef
Andrei Ruk <sloninc@mail.ru>
2023-07-15 17:42:57 +0300

    Оптимизирована логика вывода уравнения.

--- Program.cs
+++ Program.cs
@@ -9,14 +9,14 @@ namespace HW9
         /// Исходное положение стрелки меню
         /// </summary>
         private static int selectedValue = 0;
+        private static ConsoleKeyInfo ki;
+        private static bool ainput = true, binput = true, cinput = true;
+        private static double a = 0, b = 0, c = 0;
         static void Main()
         {
             try
             {
-                ConsoleKeyInfo ki;
-                bool ainput = true, binput = true, cinput = true;
                 selectedValue = 1;
-                double a = 0, b = 0, c = 0,preb=0;
                 PrintMenu();
                 WriteCursor(selectedValue);
                 do
@@ -33,144 +33,7 @@ namespace HW9
                             SetDown();
                             break;
                         default:
-                            Console.SetCursorPosition(0, selectedValue);
-                            Console.WriteLine(new string(' ', 60));
-                            Console.SetCursorPosition(0, selectedValue);
-                            Console.Write($" {options[selectedValue - 1]}: {ki.KeyChar.ToString()}");
-                            if (selectedValue == 1)
-                            {
-                                a = double.Parse(ki.KeyChar.ToString() + Console.ReadLine());
-                                Console.SetCursorPosition(0, 0);
-                                Console.WriteLine(new string(' ', 60));
-                                Console.SetCursorPosition(0, 0);
-                                if (Regex.IsMatch(equality, @"^\S+\*x\^2"))
-                                {
-                                    if (a == 0)
-                                    {
-                                        Regex regex1 = new Regex(@"^\S+\*x\^2\+?");
-                                        equality = regex1.Replace(equality, "");
-                                    }
-                                    else
-                                    {
-                                        Regex regex = new Regex(@"^\S+\*x\^2");
-                                        equality = regex.Replace(equality, $"{a}*x^2");
-                                    }
-                                }
-                                else
-                                {
-                                    if (a == 0)
-                                    {
-                                        if (Regex.IsMatch(equality, @"^-?x\^2"))
-                                        {
-                                            Regex regex = new Regex(@"^-?x\^2\+?");
-                                            equality = regex.Replace(equality, $"");
-                                        }
-                                    }
-                                    else
-                                    {
-                                        if (Regex.IsMatch(equality, @"^-?x\^2"))
-                                        {
-                                            Regex regex = new Regex(@"^-?x\^2");
-                                            equality = regex.Replace(equality, $"{a}*x^2");
-                                        }
-                                        else equality = Regex.IsMatch(equality, @"^-\w") ? equality.Insert(0, $"{a}*x^2") : equality.Insert(0, $"{a}*x^2+");
-                                    }
-                                    
-                                }
-                                if (a == 1 || a == -1)
-                                    equality = (a == 1) ? equality.Replace("1*x^", "x^") : equality.Replace("-1*x^", "-x^");
-                                Console.WriteLine(equality);
-                                ainput = false;
-                            }
-                            else if(selectedValue == 2)
-                            {
-                                
-                                b = double.Parse(ki.KeyChar.ToString() + Console.ReadLine());
-                                Console.SetCursorPosition(0, 0);
-                                Console.WriteLine(new string(' ', 60));
-                                Console.SetCursorPosition(0, 0);
-                                Regex regex = (preb==1||preb==-1)? new Regex(@"(\+|\-)?x(?!\^)") : new Regex(@"(\+|\-)?(b?|[0-9]*[.,]?[0-9]+)\*x(?!\^)");
-                                if (Regex.IsMatch(equality, @"x[^^]"))
-                                {
-                                    if (b == 0)
-                                    {
-                                        if (a == 0&& !ainput)
-                                            equality = cinput ? "c=0":$"{c}=0";
-                                        else
-                                            equality = regex.Replace(equality, "");
-                                    }
-                                    else
-                                    {
-                                        if(a == 0 && !ainput)
-                                        {
-                                            equality = (b > 0) ? regex.Replace(equality, (b == 1) ? $"x" : $"{b}*x") : regex.Replace(equality, (b == -1) ? $"-x" : $"{b}*x");
-                                        }
-                                        else
-                                        {
-                                            equality = (b > 0) ? regex.Replace(equality, (b == 1) ? $"+x" : $"+{b}*x") : regex.Replace(equality, (b == -1) ? $"-x" : $"{b}*x");
-                                        }
-                                    }
-                                }
-                                else
-                                {
-                                    if (b == 0) { }
-                                    else
-                                    {
-                                        if (a == 0 && !ainput)
-                                            equality = (c > 0||cinput) ? equality.Insert(0, $"{b}*x+") : equality.Insert(0, $"{b}*x");
-
-                                        else if(ainput)
-                                            equality = (b > 0) ? equality.Insert("a*x^2".Length, $"+{b}*x") : equality.Insert("a*x^2".Length, $"{b}*x");
-                                        else
-                                            equality = equality.Insert($"{a}*x^2".Length, $"+{b}*x");
-                                    }
-
-                                }
-                                if (b == 1||b==-1)
-                                {
-                                    equality = (b==1) ? regex.Replace(equality, "+x") : regex.Replace(equality, "-x");
-                                }
-                                Console.WriteLine(equality);
-                                preb = b;
-                                binput = false;
-                            }
-                            else
-                            {
-                                c = double.Parse(ki.KeyChar.ToString() + Console.ReadLine());
-                                Console.SetCursorPosition(0, 0);
-                                Console.WriteLine(new string(' ', 60));
-                                Console.SetCursorPosition(0, 0);
-                                Regex regex = new Regex(@"(\+|\-)+(c?|[0-9]*[.,]?[0-9]+)=");
-                                if (Regex.IsMatch(equality, @"(\+|\-)+(c?|[0-9]*[.,]?[0-9]+)="))
-                                {
-                                    if (c == 0) 
-                                    {
-                                            equality = regex.Replace(equality, "=");
-                                    }
-                                    else 
-                                    {
-                                        if (a == 0 && !ainput && b==0 && !binput)
-                                        {
-                                            equality = regex.Replace(equality, $"{c}=");
-                                        }
-                                        else
-                                        {
-                                            equality = (c > 0) ? regex.Replace(equality, $"+{c}=") : regex.Replace(equality, $"{c}=");
-                                        }
-                                    }
-                                }
-                                else
-                                {
-                                    if (c == 0) { }
-                                    else
-                                    {
-                                        equality = (c > 0) ? equality.Insert(equality.IndexOf('='), $"+{c}") : equality.Insert(equality.IndexOf('='), $"{c}");
-                                    }
-
-                                }
-                                Console.WriteLine(equality);
-                                cinput = false;
-                            }
+                            PrintEquality();
                             break;
                     }
                     WriteCursor(selectedValue);
@@ -192,6 +55,87 @@ namespace HW9
                 Console.WriteLine(fe.Message);
             }
             Console.ReadLine();
+        }
+
+        private static void PrintEquality()
+        {
+            ClearString(selectedValue);
+            Console.Write($" {options[selectedValue - 1]}: {ki.KeyChar.ToString()}");
+            switch (selectedValue)
+            {
+                case 1:
+                    a = double.Parse(ki.KeyChar.ToString() + Console.ReadLine());
+                    ClearString(0);
+                    string aword = $"{a}*x^2";
+                    Regex areg = new Regex(@"^\S*\*?x\^2");
+                    if (Regex.IsMatch(equality, @"^\S*\*?x\^2"))
+                    {
+                        equality = (a == 0) ? areg.Replace(equality, "") : areg.Replace(equality, aword);
+                        if (equality[0] == '+')
+                            equality = equality.Substring(1);
+                    }
+                    else
+                    {
+                        if (a == 0) { }
+                        else equality = Regex.IsMatch(equality, @"^-\w") ? equality.Insert(0, aword) : equality.Insert(0, aword + "+");
+                    }
+                    if (a == 1 || a == -1)
+                        equality = (a == 1) ? equality.Replace("1*x^", "x^") : equality.Replace("-1*x^", "-x^");
+                    Console.WriteLine(equality);
+                    ainput = false;
+                    break;
+                case 2:
+                    b = double.Parse(ki.KeyChar.ToString() + Console.ReadLine());
+                    ClearString(0);
+                    string bword = (b > 0) ? $"+{b}*x" : $"{b}*x";
+                    Regex breg = new Regex(@"(\+|\-)?(b?|[0-9]*[.,]?[0-9]*)\*?x(?!\^)");
+                    if (Regex.IsMatch(equality, @"x[^^]"))
+                    {
+                        equality = (b == 0) ? breg.Replace(equality, "") : breg.Replace(equality, bword);
+                        if (equality[0] == '+')
+                            equality = equality.Substring(1);
+                    }
+                    else
+                    {
+                        if (b == 0) { }
+                        else
+                        {
+                            string bcword = c > 0 || cinput ? equality.Insert(0, $"{b}*x+") : equality.Insert(0, $"{b}*x");
+                            equality = (a == 0 && !ainput) ? bcword : ainput ? equality.Insert("a*x^2".Length, bword) : equality.Insert($"{a}*x^2".Length, bword);
+                        }
+                    }
+                    if (b == 1 || b == -1)
+                        equality = (b == 1) ? equality.Replace("1*x", "x") : equality.Replace("-1*x", "-x");
+                    Console.WriteLine(equality);
+                    binput = false;
+                    break;
+                case 3:
+                    c = double.Parse(ki.KeyChar.ToString() + Console.ReadLine());
+                    ClearString(0);
+                    Regex creg = new Regex(@"(\+|\-)+(c?|[0-9]*[.,]?[0-9]+)=");
+                    if (Regex.IsMatch(equality, @"(\+|\-)+(c?|[0-9]*[.,]?[0-9]+)="))
+                    {
+                        string cword = c > 0 ? creg.Replace(equality, $"+{c}=") : creg.Replace(equality, $"{c}=");
+                        if (a == 0 && !ainput && b == 0 && !binput)
+                            equality = creg.Replace(equality, $"{c}=");
+                        else equality = c == 0 ? creg.Replace(equality, "=") : cword;
+                    }
+                    else
+                    {
+                        if (c == 0) { }
+                        else
+                            equality = (c > 0) ? equality.Insert(equality.IndexOf('='), $"+{c}") : equality.Insert(equality.IndexOf('='), $"{c}");
+                    }
+                    Console.WriteLine(equality);
+                    cinput = false;
+                    break;
+            }
+        }
+        private static void ClearString(int numstring)
+        {
+            Console.SetCursorPosition(0, numstring);
+            Console.WriteLine(new string(' ', 60));
+            Console.SetCursorPosition(0, numstring);
         }
         /// <summary>
         /// Опции меню
